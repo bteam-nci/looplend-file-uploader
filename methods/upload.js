4756c40569ccca21196365b7e141e80db83b5398
SirFrancis <borrellifrn@gmail.com>
2023-07-11 13:38:04 +0100

    first commit

---
+++ methods/upload.js
@@ -0,0 +1,27 @@
+const AWS = require('aws-sdk');
+
+module.exports.createUpload = async function(event, context){
+	// create preSigned upload link for s3
+	// return url
+	const s3 = new AWS.S3();
+	const bucketName = process.env.BUCKET_NAME;
+	const name = JSON.parse(event.body).name;
+	const key = `uploads/${name}`;
+
+	const params = {
+		Bucket: bucketName,
+		Key: key,
+		Expires: 5 * 60,
+		ContentType: 'application/octet-stream',
+		ACL: 'public-read',
+	};
+
+	const url = await s3.getSignedUrlPromise('putObject', params);
+
+	return {
+		statusCode: 200,
+		body: JSON.stringify({
+			url,
+		})
+	}
+}
