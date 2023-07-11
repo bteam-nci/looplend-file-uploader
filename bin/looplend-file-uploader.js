3b271a7b74f2573ec3013d9193e04bc1e78d1c22
SirFrancis <borrellifrn@gmail.com>
2023-07-11 14:11:09 +0100

    fix

--- bin/looplend-file-uploader.js
+++ bin/looplend-file-uploader.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
+import cdk from "aws-cdk-lib";
+import {LooplendFileUploaderStack} from "../lib/looplend-file-uploader-stack";
 
-const cdk = require('aws-cdk-lib');
-const { LooplendFileUploaderStack } = require('../lib/looplend-file-uploader-stack');
 const env = { account: '925477059004', region: 'eu-west-1' }
 
 const app = new cdk.App();
